312c7e677a1702eaf30f040e3125c9d9de6e289e
Ezequiel <74028574+Ezequiel-GEO@users.noreply.github.com>
2021-03-22 13:56:27 -0300

    Update Dados.py

--- Dados.py
+++ Dados.py
@@ -1,8 +1,13 @@
 import random
 
-a = random.randint(1,7)
-b = random.randint(1,7)
+a = random.randint(1,6)
+b = random.randint(1,6)
 c = a + b
+
+d = random.randint(1,6)
+e = random.randint(1,6)
+f = d + e
+
 
 dado = input("Quiere tirar los dados? (s)i o (n)o?")
 
@@ -18,8 +23,8 @@ dado = input("Quiere tirar los dados? (s)i o (n)o?")
 
 if dado == "s":
     print("Dale, tiremos los dados!")
-    print("El primer dado dio un valor de:", a)
-    print("El segundo dado dio un valor de:", b)
-    print("La suma da:", c)
+    print("El primer dado dio un valor de:", d)
+    print("El segundo dado dio un valor de:", e)
+    print("La suma da:", f)
 else:
     print() 
